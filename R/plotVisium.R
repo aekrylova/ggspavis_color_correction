d7e37b7bada82c3b629c61b780a230d22682f3ca
aekrylova <aekrylova@mdanderson.org>
2026-04-10 17:47:13 -0500

    Update plotVisium.R

--- R/plotVisium.R
+++ R/plotVisium.R
@@ -285,8 +285,8 @@ plotVisium <- function(spe,
                       fill = if (!is.null(annotate)) get(annotate) else NULL, 
                       col = get("highlight"))) + 
     labs(fill = annotate) + 
-    images + points + highlights + scale + 
-    coord_fixed(xlim = xlim, ylim = ylim) 
+    images + points + highlights + 
+    coord_fixed(xlim = xlim, ylim = ylim) + scale_color_manual(NULL, values=c("#A71B4B" "#E5610A" "#F6AD3E" "#FDE896" "#D0F4B1" "#52CFB0" "#0099B5" "#584B9F"))
   
   if (show_axes) {
     p <- p + 
